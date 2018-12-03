52658fb58608be556b700771058b78a985085635
jspfei <jiangfeifan8@163.com>
2018-12-03 15:18:19 +0800

    提交

---
+++ ademo/src/js/entry.js
@@ -0,0 +1,3 @@
+var React=require('react');
+var ReactDOM=require('react-dom');
+ReactDOM.render(<div>Hello world!</div>, document.getElementById("test"));
---
+++ basicH5/ajax/demo_post.php
@@ -0,0 +1,2 @@
+<p style='color:red;'>本内容是使用 POST 方法请求的。</p><p style='color:red;'>请求时间：
+2018-11-29 13:16:44</p>
